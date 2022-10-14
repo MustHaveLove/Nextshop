dc6e5cb6a65f9146786b5212c8f4fbbf34a58c35
MustHaveLove <wodnjs6159@gmail.com>
2022-10-14 09:55:50 +0900

    3.8

--- pages/cart.js
+++ pages/cart.js
@@ -5,8 +5,10 @@ import React, { useContext } from "react";
 import Layout from "../components/Layout";
 import { Store } from "../utils/Store";
 import { XCircleIcon } from "@heroicons/react/outline";
+import dynamic from 'next/dynamic'
 
-export default function CartScreen() {
+
+function CartScreen() {
   const router = useRouter();
   const { state, dispatch } = useContext(Store);
 
@@ -107,3 +109,5 @@ export default function CartScreen() {
     </Layout>
   );
 }
+
+export default dynamic(() => Promise.resolve(CartScreen), {ssr: false})

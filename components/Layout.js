80f065a2bf060f571dff2ad5891b95cf1e7dbe2a
MustHaveLove <wodnjs6159@gmail.com>
2022-11-04 10:36:09 +0900

    logout

--- components/Layout.js
+++ components/Layout.js
@@ -1,25 +1,33 @@
-import Head from "next/head";
-import Link from "next/link";
-import { React, useContext, useEffect, useState } from "react";
-import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css'
-import { Store } from "../utils/Store";
-import { useSession } from 'next-auth/react'
-
+import Head from 'next/head';
+import Link from 'next/link';
+import { React, useContext, useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Store } from '../utils/Store';
+import { signOut, useSession } from 'next-auth/react';
+import { Menu } from '@headlessui/react';
+import DropdownLink from './DropdownLink';
+import Cookies from 'js-cookie';
 
 export default function Layout({ title, children }) {
-  const { status, data: session } = useSession()
-  const { state } = useContext(Store);
+  const { status, data: session } = useSession();
+  const { state, dispatch } = useContext(Store);
   const { cart } = state;
-  const [cartItemsCount, setCartItemsCount] = useState(0)
+  const [cartItemsCount, setCartItemsCount] = useState(0);
   useEffect(() => {
-    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0))
-  }, [cart.cartItems])
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+
+  const logoutClickHandler = () => {
+    Cookies.remove('cart');
+    dispatch({ type: 'CART_RESET' });
+    signOut({ callbackUrl: '/login' });
+  };
 
   return (
     <>
       <Head>
-        <title>{title ? title + " - NextShop" : "NextShop"}</title>
+        <title>{title ? title + ' - NextShop' : 'NextShop'}</title>
         <meta name="description" content="Nextjs" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -31,7 +39,7 @@ export default function Layout({ title, children }) {
               <a className="text-lg font-bold">NextShop</a>
             </Link>
             <div>
-            <Link href="/profile">
+              <Link href="/profile">
                 <a className="p-2">Profile</a>
               </Link>
               <Link href="/cart">
@@ -45,16 +53,42 @@ export default function Layout({ title, children }) {
                 </a>
               </Link>
               {status === 'loading' ? (
-              'Loading'
-               ) : session?.user ? (
-                session.user.name
-                ) : (
-
-
-              <Link href="/login">
-                <a className="p-2">Login</a>
-              </Link>
-                )}
+                'Loading'
+              ) : session?.user ? (
+                <Menu as="div" className="relative inline-block">
+                  <Menu.Button className="text-blue-600">
+                    {session.user.name}
+                  </Menu.Button>
+                  <Menu.Items className="absolute right-0 w-56 origin-top-right bg-white  shadow-lg ">
+                    <Menu.Item>
+                      <DropdownLink className="dropdown-link" href="/profile">
+                        Profile
+                      </DropdownLink>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <DropdownLink
+                        className="dropdown-link"
+                        href="/order-history"
+                      >
+                        Order History
+                      </DropdownLink>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <a
+                        className="dropdown-link"
+                        href="#"
+                        onClick={logoutClickHandler}
+                      >
+                        Logout
+                      </a>
+                    </Menu.Item>
+                  </Menu.Items>
+                </Menu>
+              ) : (
+                <Link href="/login">
+                  <a className="p-2">Login</a>
+                </Link>
+              )}
             </div>
           </nav>
         </header>
